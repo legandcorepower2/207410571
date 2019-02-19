a42d4edf3190d365bc57ee306d8ca71180437928
legandcorepower2 <43173888+legandcorepower2@users.noreply.github.com>
2019-02-19 20:43:46 +0800

    Add files via upload

---
+++ 1N/WEEK1/WEEK1-3/app.js
@@ -0,0 +1,35 @@
+//string
+
+const name = '1234';
+console.log(typeof name);
+
+//number
+const age=18;
+console.log(typeof age);
+
+
+//boolean
+const haskids = true; //true, false
+console.log(typeof haskids);
+
+//null
+const car =null;
+console.log(typeof car);
+
+//undefined
+let test;
+console.log(typeof test);
+
+//symbol
+const sym= Symbol();
+console.log(typeof sym);
+//object
+
+//array
+const hobbiess = ['movies','music'];
+console.log(typeof hobbies);
+
+//objects
+const today = new Date();
+console.log(today);
+console.log(typeof today);
