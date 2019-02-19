0da42a08702ec7b61dad49c99d678f61aee423f0
legandcorepower2 <43173888+legandcorepower2@users.noreply.github.com>
2019-02-19 20:10:10 +0800

    Add files via upload

---
+++ 1N/WEEK1/WEEK1-2/app.js
@@ -0,0 +1,54 @@
+// var
+
+var name ="鄭凱鴻";
+console.log(name);
+
+name = '1234';
+console.log(name);
+
+name = '2019';
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting = 'hello';
+console.log(greeting);
+
+//letter,number
+//annot start with number
+
+//Multi word vars
+var fristname1 ='鄭凱鴻';
+var fristname2 ='john';
+var fristname3 ='sara';
+var fristname4;
+
+//let
+let name2 ='鄭凱鴻';
+console.log(name2);
+name2 = '1234';
+console.log(name2);
+name2 = '2019';
+console.log(name2);
+
+// const
+// const name3 ='鄭凱鴻';
+// console.log(name3);
+// name3 = '1234';
+// console.log(name3);
+// name3 = '2019';
+// console.log(name3);
+
+const person ={
+    name: '123',
+    age: 18
+}
+person.name = '鄭凱鴻';
+person.age = '19';
+console.log(person);
+
+const numbers = [1,2,3,5,8,13,21]
+numbers.push(34);
+console.log(numbers);
+
+
