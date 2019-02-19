8689ca023bd84f51209f73d92d6a10767d040444
legandcorepower2 <43173888+legandcorepower2@users.noreply.github.com>
2019-02-19 19:18:07 +0800

    Add files via upload

---
+++ 1N/WEEK1/app.js
@@ -0,0 +1,23 @@
+console.log('Hello World');
+console.log(1213); //id
+console.log(true);
+
+var greeting ="鄭凱鴻"; //name
+console.log(greeting);
+console.log([2019,2,19]); //today date
+console.log({firstName:'凱鴻',lastName:'鄭'}); //object
+console.table({firstName:'凱鴻',lastName:'鄭'});
+
+console.error('This is some error');
+console.warn('This is some warning');
+
+console.time('hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('hello');
+
